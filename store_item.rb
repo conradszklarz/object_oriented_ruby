47a547d2efa93b056a578a3c398be6cc8a30c053
conradskittles <conradszklarz@gmail.com>
2018-05-01 16:41:35 -0500

    added information

---
+++ store_item.rb
@@ -0,0 +1,21 @@
+store_item1 = { color: "blue", price: 5.50, fruit: "blueberries" }
+
+
+store_item2 = {color: "red", price: 3.00, fruit: "strawberries" }
+
+
+store_item3 = {color: "green", price: 2.45, fruit: "celery" }
+
+
+p "our blueberries are blue and cost 5.50"
+puts "our #{store_item1[:fruit]} are #{store_item1[:color]} and cost #{store_item1[:price]}"
+
+p "our celery is green and cost 2.45"
+puts "our #{store_item3[:fruit]} is #{store_item3[:color]} and costs #{store_item3[:price]} "
+
+p "our strawberries are red and cost 3.00"
+puts "our #{store_item2[:fruit]} are #{store_item2[:color]} and cost #{store_item2[:price]}"
+
+
+
+
