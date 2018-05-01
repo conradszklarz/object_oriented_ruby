2beabbbd4b63dffcb358b38868cf9cf8ad32078d
conradskittles <conradszklarz@gmail.com>
2018-05-01 17:22:45 -0500

    added info

--- store_item.rb
+++ store_item.rb
@@ -1,32 +1,28 @@
-store_item1 = { color: "blue", price: 5.50, fruit: "blueberries" }
-
-
-store_item2 = {color: "red", price: 3.00, fruit: "strawberries" }
-
-
-store_item3 = {color: "green", price: 2.45, fruit: "pears" }
-
-
-p "our blueberries are blue and cost 5.50"
-puts "our #{store_item1[:fruit]} are #{store_item1[:color]} and cost #{store_item1[:price]}"
-
-p "our celery is green and cost 2.45"
-puts "our #{store_item3[:fruit]} is #{store_item3[:color]} and costs #{store_item3[:price]} "
-
-p "our strawberries are red and cost 3.00"
-puts "our #{store_item2[:fruit]} are #{store_item2[:color]} and cost #{store_item2[:price]}"
-
-class Store_item
+class StoreItem
   attr_reader :fruit, :color, :price
   attr_writer :fruit, :color, :price
 
-  def initialize(input_fruit, input_color, input_price)
-    @fruit = input_fruit
-    @color = input_color
-    @price = input_price
+  def initialize(input_options)
+    @fruit = input_options[:fruit]
+    @color = input_options[:color]
+    @price = input_options[:price]
+  end
+
+  def print_info
+    puts "our #{@fruit} are #{@color} and cost #{@price}"
   end
 end
 
 
+store_item_1 = StoreItem.new(color: "blue", price: 5.50, fruit: "blueberries" )
 
+
+store_item_2 = StoreItem.new(color: "red", price: 3.00, fruit: "strawberries" )
+
+
+store_item_3 = StoreItem.new(color: "green", price: 2.45, fruit: "pears" )
+
+store_item_1.print_info
+store_item_2.print_info
+store_item_3.print_info
 
